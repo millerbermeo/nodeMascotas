9aa07e7d7a5215d12bde7a4dc6021f5c45b7d81f
millerbermeo <millerbermeo30@gmail.com>
2024-08-07 22:10:43 -0500

    controlador mascotas adopcion rows

--- src/controllers/adopcionesController.js
+++ src/controllers/adopcionesController.js
@@ -36,7 +36,7 @@ export const getAdopcion = async (req, res) => {
     if (rows.length === 0) {
       return res.status(404).json({ message: 'Adopcion not found' });
     }
-    res.json(rows[0]);
+    res.json(rows);
   } catch (error) {
     console.error('Error fetching adopcion: ', error);
     res.status(500).json({ message: 'Error fetching adopcion', error: error.message });
